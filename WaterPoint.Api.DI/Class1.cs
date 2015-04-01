5ab87e1c3316cf31791a4a11bca0717e8e9a03f8
Walter Huang <walter.huang@xero.com>
2015-04-02 11:25:07 +1300

    add ninject pipeline in api

---
+++ WaterPoint.Api.DI/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject.Modules;
+
+namespace WaterPoint.Api.DI
+{
+    public class ApiDiModule: NinjectModule
+    {
+        public override void Load()
+        {
+            
+        }
+    }
+}
---
+++ WaterPoint.Api/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Owin;
+using Ninject;
+using Owin;
+//using Microsoft.Owin.Cors;
+//using Microsoft.Owin.Security.Infrastructure;
+//using Microsoft.Owin.Security.OAuth;
+using Ninject.Web.Common.OwinHost;
+using Ninject.Web.WebApi.OwinHost;
+
+[assembly: OwinStartup(typeof(WaterPoint.Api.Startup))]
+
+namespace WaterPoint.Api
+{
+    public class Startup
+    {
+        public IKernel CreateCernel()
+        {
+            var kernel = new StandardKernel(new WaterPoint.Api.DI.ApiDiModule());
+
+            return kernel;
+
+        }
+
+        public void Configuration(IAppBuilder app)
+        {
+            var config = new HttpConfiguration();
+
+            app.UseNinjectMiddleware(() => CreateCernel())
+                .UseNinjectWebApi(config);
+        }
+    }
+}
