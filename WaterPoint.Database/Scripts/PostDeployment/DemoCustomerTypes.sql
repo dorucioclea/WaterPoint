c0e086da15b385d9f6e9c4ebf02fd94a9d242757
Walter Huang <walter.huang@xero.com>
2015-11-04 22:09:16 +1300

    more

---
+++ WaterPoint.Database/Scripts/PostDeployment/DemoCustomerTypes.sql
@@ -0,0 +1,25 @@
+﻿
+DECLARE @orgId INT = (SELECT Id FROM Organization WHERE Name = 'Water Point')
+
+IF NOT EXISTS(SELECT TOP 1 * FROM dbo.CustomerType WHERE OrganizationId = @orgId)
+BEGIN
+    INSERT INTO dbo.CustomerType
+    (OrganizationId,Description,NameDisplayStrategy)
+    VALUES
+    (@orgId,N'标准','/**LastName**/ /**OtherName**/ /**FirstName**/')
+
+    INSERT INTO dbo.CustomerType
+    (OrganizationId,Description,NameDisplayStrategy)
+    VALUES
+    (@orgId,N'特殊','/**LastName**/ /**OtherName**/ /**FirstName**/')
+
+    INSERT INTO dbo.CustomerType
+    (OrganizationId,Description,NameDisplayStrategy)
+    VALUES
+    (@orgId,N'个人','/**LastName**/ /**OtherName**/ /**FirstName**/')
+
+    INSERT INTO dbo.CustomerType
+    (OrganizationId,Description,NameDisplayStrategy)
+    VALUES
+    (@orgId,N'公司','/**LastName**/ /**OtherName**/ /**FirstName**/')
+END
