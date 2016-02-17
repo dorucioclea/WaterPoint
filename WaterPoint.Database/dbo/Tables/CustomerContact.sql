80cc34c254607f87a3dcf8cee140a823f687ebc0
hmiaosys <hmiaosys@gmail.com>
2016-02-17 21:08:32 +1300

    fix the name of the index

--- WaterPoint.Database/dbo/Tables/CustomerContact.sql
+++ WaterPoint.Database/dbo/Tables/CustomerContact.sql
@@ -5,5 +5,5 @@
     [IsPrimary] BIT NOT NULL DEFAULT 0,
     PRIMARY KEY ([ContactId], [CustomerId]),
     CONSTRAINT [FK_dbo_CustomerContact_dbo_Customer] FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customer]([Id]),
-    CONSTRAINT [FK_dbo_CustomerContact_dbo_Address] FOREIGN KEY ([ContactId]) REFERENCES [dbo].[Contact]([Id])
+    CONSTRAINT [FK_dbo_CustomerContact_dbo_Contact] FOREIGN KEY ([ContactId]) REFERENCES [dbo].[Contact]([Id])
 )
